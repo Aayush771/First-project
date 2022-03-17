21ad3022d4a54cd713eb871b3a4edc8067e750d5
Aayush771 <97461330+Aayush771@users.noreply.github.com>
2022-03-17 06:44:31 +0530

    java files

--- src/com/masai/unit_4/Evaluation_C4/Q2/StringManipulator.java
+++ src/com/masai/unit_4/Evaluation_C4/Q2/StringManipulator.java
@@ -3,36 +3,15 @@ package com.masai.unit_4.Evaluation_C4.Q2;
 import java.util.Scanner;
 
 public class StringManipulator {
-    public String removeVowels(String str)
-    {
-      StringBuilder str1= new StringBuilder();
-      for(int i=0;i<str.length();i++){
-         if(str.charAt(i) =='a'){
-             continue;
-         }
-         else if(str.charAt(i) !='e'){
-           continue;
-          }
-          else if(str.charAt(i) !='i'){
-            continue;
-          }
-          else if(str.charAt(i) !='o'){
-              continue;
-          }
-          else if(str.charAt(i) !='u'){
-           continue;
-          }
-          else{
-              str1.append(str.charAt(i));
-         }
-      }
-      return str1.toString();
+    public static void main(String[] args){
+            Scanner Sc = new Scanner(System.in);
+            String s1 = Sc.next();
+            StringManipulator m1 = new StringManipulator();
+            System.out.println(m1.removeVowels(s1));
+        }
+
+
+    public String removeVowels(String str) {
+        return str.replaceAll("[aeiouAEIOU]", "");
     }
-    public static void main(String[] args) {
-        Scanner Sc=new Scanner(System.in);
-        String s1=Sc.next();
-      StringManipulator s2=new StringManipulator();
-      String i=s2.removeVowels(s1);
-        System.out.println(i);
     }
-}
--- src/com/masai/unit_4/Evaluation_C4/Q3/Main.java
+++ src/com/masai/unit_4/Evaluation_C4/Q3/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void methodIncrement(int a,int b,int c){
-        for(int i=a;i<b;i=i+c){
+        for(int i=a;i<=b;i=i+c){
             System.out.println(i);
         }
     }
@@ -14,11 +14,12 @@ public class Main {
         int start=SC.nextInt();
         int end=SC.nextInt();
         int inc=SC.nextInt();
-       if(end>start){
+       if(start>end){
            System.out.println("Stirng error");
        }
        else {
            methodIncrement(start, end, inc);
        }
+
     }
 }
